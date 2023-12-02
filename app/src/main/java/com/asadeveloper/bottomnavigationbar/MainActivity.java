dcee1b16812ca88d17e1d1bca8d145272f3ed189
SaqibAA <ansarisaqib8286@gmail.com>
2023-12-02 11:09:52 +0530

    Initial

---
+++ app/src/main/java/com/asadeveloper/bottomnavigationbar/MainActivity.java
@@ -0,0 +1,47 @@
+package com.asadeveloper.bottomnavigationbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.asadeveloper.bottomnavigationbar.databinding.ActivityMainBinding;
+import com.asadeveloper.bottomnavigationbar.fragment.HomeFragment;
+import com.asadeveloper.bottomnavigationbar.fragment.ProfileFragment;
+import com.asadeveloper.bottomnavigationbar.fragment.SettingFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.navBar.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            } else if (itemId == R.id.setting) {
+                replaceFragment(new SettingFragment());
+            }
+
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
